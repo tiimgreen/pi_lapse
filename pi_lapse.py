b6dab391ae00d6785c16a2350eb1559b36a0dc95
Tim Green <tiimgreen@gmail.com>
2014-06-08 23:04:20 +0100

    Add start info

--- pi_lapse.py
+++ pi_lapse.py
@@ -13,8 +13,8 @@ time_between_frames = 60
 duration = 86400
 
 # Image Dimensions (pixels)
-image_height = 972
 image_width = 1296
+image_height = 972
 
 total_frames = duration / time_between_frames
 
@@ -25,6 +25,9 @@ def capture_image():
     print("Captured Image %d of %d, named: %s" % (frame_counter, total_frames, filename))
 
 last_capture = datetime.now()
+
+print("========== PiLapse Started ==========")
+print("A photo will be taken every %d seconds for the next %d seconds." % (time_between_frames, duration))
 
 while frame_counter < total_frames:
     if last_capture < (datetime.now() - timedelta(seconds = time_between_frames)):
